f6c80adbd5ddfee9106661542da66d98d173f0d8
RohitRaut213 <137475686+RohitRaut213@users.noreply.github.com>
2023-07-26 20:58:23 +0530

    Assisgnment1.py

--- Assisgnment1.py
+++ Assisgnment1.py
@@ -1,5 +1,6 @@
-n1 = 10
-n2 = 20
-n3 = 30
-avg = (n1 +n2 + n3) / 3
-print("The average of" n1, n2, n3 "is",avg)
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+n3 = int(input("Enter the third number: "))
+
+avg = (n1 + n2 + n3) / 3
+print("The average of", n1, n2, n3, "is", avg)
